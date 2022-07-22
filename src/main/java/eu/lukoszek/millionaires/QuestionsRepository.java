cdc601a34d721211d120cebeebc64f1bff4f4f14
Marek Lukoszek <m.lukoszek@wide-open.pl>
2022-07-22 09:32:52 +0200

    QuestionsService, evaluation.html.

--- src/main/java/eu/lukoszek/millionaires/QuestionsRepository.java
+++ src/main/java/eu/lukoszek/millionaires/QuestionsRepository.java
@@ -3,5 +3,9 @@ package eu.lukoszek.millionaires;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface QuestionsRepository extends CrudRepository<Questions, Long> { }
+public interface QuestionsRepository extends CrudRepository<Questions, Long> {
+    List<Questions> findAllByDifficulty(int difficulty);
+}
