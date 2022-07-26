bd8a1260452e9d68664c7a619d0058290ff8107f
Marek Lukoszek <m.lukoszek@wide-open.pl>
2022-07-26 10:03:53 +0200

    Dto, DtoMapper, QuestionController (session implementation)

---
+++ src/main/java/eu/lukoszek/millionaires/QuestionDto.java
@@ -0,0 +1,96 @@
+package eu.lukoszek.millionaires;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Component
+public class QuestionDto {
+    Long questionId;
+    String questionBody;
+    String answerA;
+    String answerB;
+    String answerC;
+    String answerD;
+    char rightAnswer;
+    int difficulty;
+
+    public QuestionDto(Long questionId, String questionBody, String answerA, String answerB, String answerC, String answerD, char rightAnswer, int difficulty) {
+        this.questionId = questionId;
+        this.questionBody = questionBody;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.answerD = answerD;
+        this.rightAnswer = rightAnswer;
+        this.difficulty = difficulty;
+    }
+
+    public QuestionDto() {
+    }
+
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestionBody() {
+        return questionBody;
+    }
+
+    public void setQuestionBody(String questionBody) {
+        this.questionBody = questionBody;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public void setAnswerD(String answerD) {
+        this.answerD = answerD;
+    }
+
+    public char getRightAnswer() {
+        return rightAnswer;
+    }
+
+    public void setRightAnswer(char rightAnswer) {
+        this.rightAnswer = rightAnswer;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+}
--- src/main/java/eu/lukoszek/millionaires/QuestionsController.java
+++ src/main/java/eu/lukoszek/millionaires/QuestionsController.java
@@ -1,11 +1,13 @@
 package eu.lukoszek.millionaires;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 
 import static java.lang.Integer.parseInt;
@@ -13,30 +15,38 @@ import static java.lang.Integer.parseInt;
 
 @Controller
 class QuestionsController {
-    QuestionsService questionsService;
+    private QuestionsService questionsService;
+    private QuestionDto questionDto;
 
-    public QuestionsController(QuestionsService questionsService) {
+    public QuestionsController(QuestionsService questionsService, QuestionDto questionDto) {
         this.questionsService = questionsService;
+        this.questionDto = questionDto;
     }
 
     @RequestMapping("/drawQuestion")
-    String getParameter(@RequestParam String start, Model model) {
-        model.addAttribute("name", start);
-        Questions question = questionsService.drawQuestion(parseInt(start));
-        model.addAttribute("questionBody", question.getQuestionBody());
-        model.addAttribute("answerA", question.getAnswerA());
-        model.addAttribute("answerB", question.getAnswerB());
-        model.addAttribute("answerC", question.getAnswerC());
-        model.addAttribute("answerD", question.getAnswerD());
+    String getParameter(@RequestParam String start, HttpSession session, Model model) {
+        questionDto = questionsService.drawQuestion(parseInt(start + 1));
+        session.setAttribute("rightAnswer", questionDto.getRightAnswer());
+        session.setAttribute("difficulty", questionDto.getDifficulty());
+        session.setAttribute("questionId", questionDto.getQuestionId());
+        model.addAttribute("questionBody", questionDto.getQuestionBody());
+        model.addAttribute("answerA", questionDto.getAnswerA());
+        model.addAttribute("answerB", questionDto.getAnswerB());
+        model.addAttribute("answerC", questionDto.getAnswerC());
+        model.addAttribute("answerD", questionDto.getAnswerD());
+
         return "question";
     }
 
     @RequestMapping("/response")
-    String checkAnswer(@RequestParam String myAnswer, Model model){
-
-
-    return null;
+    String checkAnswer(@RequestParam String userAnswer, HttpSession session, Model model) {
+        String goodAnswer = (String) session.getAttribute("rightAnswer");
+        model.addAttribute("result", questionsService.evaluate(userAnswer, goodAnswer));
+        session.setAttribute("difficulty", session.getAttribute("difficulty" + 1));
+        System.out.println(session.getAttribute("difficulty"));
+        return "evaluation";
     }
+
     @RequestMapping("/getStats")
     @ResponseBody
     LocalDate home() {
