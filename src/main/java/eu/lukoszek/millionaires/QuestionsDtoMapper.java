cbdf28aba098977dde0dafc01f0e7863ca801cc3
Marek Lukoszek <m.lukoszek@wide-open.pl>
2022-07-26 10:03:53 +0200

    Dto, DtoMapper, QuestionController (session implementation)

---
+++ src/main/java/eu/lukoszek/millionaires/QuestionsDtoMapper.java
@@ -0,0 +1,26 @@
+package eu.lukoszek.millionaires;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class QuestionsDtoMapper {
+    private final QuestionsRepository questionsRepository;
+
+    QuestionsDtoMapper(QuestionsRepository questionsRepository) {
+        this.questionsRepository = questionsRepository;
+    }
+
+    QuestionDto map(Questions questions) {
+        QuestionDto dto = new QuestionDto();
+        dto.setQuestionId(questions.getQuestionId());
+        dto.setQuestionBody(questions.getQuestionBody());
+        dto.setAnswerA(questions.getAnswerA());
+        dto.setAnswerB(questions.getAnswerB());
+        dto.setAnswerC(questions.getAnswerC());
+        dto.setAnswerD(questions.getAnswerD());
+        dto.setRightAnswer(questions.getRightAnswer());
+        dto.setDifficulty(questions.getDifficulty());
+
+        return dto;
+    }
+}
---
+++ src/main/java/eu/lukoszek/millionaires/QuestionsService.java
@@ -0,0 +1,33 @@
+package eu.lukoszek.millionaires;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.*;
+
+@Service
+@Scope(scopeName = WebApplicationContext.SCOPE_SESSION,
+        proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class QuestionsService {
+    private QuestionsRepository questionsRepository;
+    private QuestionsDtoMapper questionsDtoMapper;
+
+    public QuestionsService(QuestionsRepository questionsRepository, QuestionsDtoMapper questionsDtoMapper) {
+        this.questionsRepository = questionsRepository;
+        this.questionsDtoMapper = questionsDtoMapper;
+    }
+
+    QuestionDto drawQuestion(int difficulty) {
+        List<Questions> questionsList = questionsRepository.findAllByDifficulty(difficulty);
+        Collections.shuffle(questionsList);
+        return questionsDtoMapper.map(questionsList.get(0));
+    }
+
+    Boolean evaluate(String userAnswer, String rightAnswer) {
+        return userAnswer.equals(rightAnswer);
+    }
+}
+
+
